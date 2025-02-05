cf8ed11882e267af796a92da07ec3d7ff7917a1e
Glen Sasek <glen.sasek@pcc.edu>
2025-02-05 19:38:24 +0000

    lab3-8 better format

--- lab3-8.py
+++ lab3-8.py
@@ -1,6 +1,9 @@
+import math
 
 n = int(input("POSITIVE INT PLEASE: "))
 print("THANK YOU")
 
+width = int(math.log(10 * n, 10)) + 1
+
 for i in range(1, 11):
-    print(i, " *", n, " =", i * n)
+    print('{:>5}'.format(i), "*", n, "=", '{:>{w}}'.format(i * n, w = width))
